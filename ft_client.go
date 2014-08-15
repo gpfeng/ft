370b34ed438357d70dbf828aa2caf459fa5cb49f
gpfeng <gpfeng.cs@gmail.com>
2014-08-16 00:39:34 +0800

    code format

--- ft_client.go
+++ ft_client.go
@@ -1,206 +1,208 @@
-// file transfer client 
+// file transfer client
 // author: fengguangpu@nibirutech.com
 
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "flag"
-    "bufio"
-    "path"
-    "strings"
-    "sync"
-    "net"
-    "log"
-    "./util"
+	"./util"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path"
+	"strings"
+	"sync"
 )
 
 func ParseParams() (host string, port int, help, compressed bool, files []string) {
-    H := flag.String("H", "127.0.0.1", "host name or ip address")
-    p := flag.Int("p", 12345, "host port")
-    c := flag.Bool("c", false, "compress or not")
-    h := flag.Bool("h", false, "print help information")
+	H := flag.String("H", "127.0.0.1", "host name or ip address")
+	p := flag.Int("p", 12345, "host port")
+	c := flag.Bool("c", false, "compress or not")
+	h := flag.Bool("h", false, "print help information")
 
-    flag.Parse()
+	flag.Parse()
 
-    host = *H
-    port = *p
-    compressed = *c
-    help = *h
-    files = flag.Args()
+	host = *H
+	port = *p
+	compressed = *c
+	help = *h
+	files = flag.Args()
 
-    return
+	return
 }
 
 // server will send only one message to client
 func ReceiveResponse(conn net.Conn, filename string, end *bool, wg *sync.WaitGroup) {
-    defer wg.Done()
-    buf := make([]byte, util.MAX_RESPONSE_SIZE)
-    l, err := conn.Read(buf)
-    if err != nil || l <= 0 {
-        log.Printf("Read() failed: %s\n", err.Error())
-    } else {
-        fields := strings.Split(string(buf), "/")
-        if len(fields) < 2 {
-            log.Printf("wrong response, must be a bug\n")
-            *end = true
-            return
-        }
-        if fields[0] == "ok" {
-            log.Printf("Sending %s successed", filename)
-        } else {
-            log.Printf("Sending %s failed: %s", filename, fields[1])
-            *end = true
-        }
-    }
+	defer wg.Done()
+	buf := make([]byte, util.MAX_RESPONSE_SIZE)
+	l, err := conn.Read(buf)
+	if err != nil || l <= 0 {
+		log.Printf("Read() failed: %s\n", err.Error())
+	} else {
+		fields := strings.Split(string(buf), "/")
+		if len(fields) < 2 {
+			log.Printf("wrong response, must be a bug\n")
+			*end = true
+			return
+		}
+		if fields[0] == "ok" {
+			log.Printf("Sending %s successed", filename)
+		} else {
+			log.Printf("Sending %s failed: %s", filename, fields[1])
+			*end = true
+		}
+	}
 }
 
 // two phase
 // 1. send file meta
 // 2. send file contents
 func Send(conn net.Conn, file *os.File, fullname string, compressed bool) {
-    // get file information
-    fi, err := file.Stat()
-    if err != nil {
-        log.Printf("Stat() %s failed: %s\n", file.Name(), err.Error())
-        return
-    }
+	// get file information
+	fi, err := file.Stat()
+	if err != nil {
+		log.Printf("Stat() %s failed: %s\n", file.Name(), err.Error())
+		return
+	}
 
-    if fi.IsDir() {
-        log.Printf("%s is a directory, not a file\n", fullname)
-        return
-    }
+	if fi.IsDir() {
+		log.Printf("%s is a directory, not a file\n", fullname)
+		return
+	}
 
-    size := fi.Size()
-    mode := fi.Mode()
+	size := fi.Size()
+	mode := fi.Mode()
 
-    md5, err := util.MD5(file)
-    if err != nil {
-       log.Printf(err.Error()) 
-       return
-    }
+	md5, err := util.MD5(file)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
 
-    meta := make([]byte, util.META_BUF_SIZE)
-    if compressed {
-        util.FormatMeta(meta, fullname, size, mode, md5, "yes", "zlib")
-    } else {
-        util.FormatMeta(meta, fullname, size, mode, md5, "no", "none")
-    }
+	meta := make([]byte, util.META_BUF_SIZE)
+	if compressed {
+		util.FormatMeta(meta, fullname, size, mode, md5, "yes", "zlib")
+	} else {
+		util.FormatMeta(meta, fullname, size, mode, md5, "no", "none")
+	}
 
-    // sent meta info
-    if _, err := conn.Write([]byte(meta)); err != nil {
-        log.Printf("Send meta failed: %s\n", err.Error())
-        return
-    }
+	// sent meta info
+	if _, err := conn.Write([]byte(meta)); err != nil {
+		log.Printf("Send meta failed: %s\n", err.Error())
+		return
+	}
 
-    // stop if server has sent response 
-    interrupted := false
+	// stop if server has sent response
+	interrupted := false
 
-    var wg sync.WaitGroup
-    wg.Add(1)
-    go ReceiveResponse(conn, fullname, &interrupted, &wg)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go ReceiveResponse(conn, fullname, &interrupted, &wg)
 
-    // send file contents
-    var sent int64 = 0
-    buf := make([]byte, util.DATA_BUF_SIZE)
-    hdr := make([]byte, util.DATA_HDR_SIZE)
+	// send file contents
+	var sent int64 = 0
+	buf := make([]byte, util.DATA_BUF_SIZE)
+	hdr := make([]byte, util.DATA_HDR_SIZE)
 
-    var fr *bufio.Reader
+	var fr *bufio.Reader
 
-    if compressed {
-        cr := util.CompressReader(file)
-        fr = bufio.NewReader(cr)
-        defer cr.Close()
-    } else {
-        fr = bufio.NewReader(file)
-    }
+	if compressed {
+		cr := util.CompressReader(file)
+		fr = bufio.NewReader(cr)
+		defer cr.Close()
+	} else {
+		fr = bufio.NewReader(file)
+	}
 
-    for {
-        if interrupted { break }
+	for {
+		if interrupted {
+			break
+		}
 
-        rlen, err := fr.Read(buf)
-        if err == nil {
-            if rlen > 0 {
-                // send header
-                util.FormatHeader(hdr, rlen)
-                hlen, e := conn.Write(hdr)
-                if e != nil || hlen != util.DATA_HDR_SIZE {
-                    log.Printf("Write() failed: %s\n", e.Error())
-                    break
-                }
-                // send data
-                wlen, e := conn.Write(buf[:rlen])
-                if e != nil || wlen != rlen {
-                    log.Printf("Write() failed: %s\n", e.Error())
-                    break
-                }
-                sent += int64(wlen)
-            }
-        } else if err == io.EOF {
-            // send header, tell the server to stop
-            util.FormatHeader(hdr, 0)
-            hlen, e := conn.Write(hdr)
-            if e != nil || hlen != util.DATA_HDR_SIZE {
-                log.Printf("Write() failed: %s\n", e.Error())
-                break
-            }
-            break
-        } else {
-            log.Printf("Read() failed: %s\n", err.Error())
-            break
-        }
-    }
+		rlen, err := fr.Read(buf)
+		if err == nil {
+			if rlen > 0 {
+				// send header
+				util.FormatHeader(hdr, rlen)
+				hlen, e := conn.Write(hdr)
+				if e != nil || hlen != util.DATA_HDR_SIZE {
+					log.Printf("Write() failed: %s\n", e.Error())
+					break
+				}
+				// send data
+				wlen, e := conn.Write(buf[:rlen])
+				if e != nil || wlen != rlen {
+					log.Printf("Write() failed: %s\n", e.Error())
+					break
+				}
+				sent += int64(wlen)
+			}
+		} else if err == io.EOF {
+			// send header, tell the server to stop
+			util.FormatHeader(hdr, 0)
+			hlen, e := conn.Write(hdr)
+			if e != nil || hlen != util.DATA_HDR_SIZE {
+				log.Printf("Write() failed: %s\n", e.Error())
+				break
+			}
+			break
+		} else {
+			log.Printf("Read() failed: %s\n", err.Error())
+			break
+		}
+	}
 
-    if !interrupted {
-        if compressed {
-            log.Printf("%s: %d bytes, compressed to %d bytes, md5: %s\n",fullname, size, sent, md5)
-        } else {
-            log.Printf("%s: %d bytes, sent: %d bytes, md5: %s\n",fullname, size, sent, md5)
-        }
+	if !interrupted {
+		if compressed {
+			log.Printf("%s: %d bytes, compressed to %d bytes, md5: %s\n", fullname, size, sent, md5)
+		} else {
+			log.Printf("%s: %d bytes, sent: %d bytes, md5: %s\n", fullname, size, sent, md5)
+		}
 
-    }
-    wg.Wait()
+	}
+	wg.Wait()
 }
 
 func SendFile(name string, compressed bool, host string, port int, wg *sync.WaitGroup) {
-    defer wg.Done()
+	defer wg.Done()
 
-    // open file
-    file, err := os.Open(name)
-    if err != nil {
-        log.Printf("Open() %s failed: %s\n", name, err.Error())
-        return
-    }
-    defer file.Close()
+	// open file
+	file, err := os.Open(name)
+	if err != nil {
+		log.Printf("Open() %s failed: %s\n", name, err.Error())
+		return
+	}
+	defer file.Close()
 
-    // connect to the server
-    conn, err := net.Dial("tcp",  fmt.Sprintf("%s:%d", host, port))
-    if err != nil {
-        log.Printf("Dial() failed: %s\n", err.Error())
-        return
-    }
-    defer conn.Close()
+	// connect to the server
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		log.Printf("Dial() failed: %s\n", err.Error())
+		return
+	}
+	defer conn.Close()
 
-    Send(conn, file, name, compressed)
+	Send(conn, file, name, compressed)
 }
 
 func main() {
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    host, port, help, compressed, files := ParseParams()
+	host, port, help, compressed, files := ParseParams()
 
-    if help {
-        fmt.Printf("Usage: %s [-Hchp] file1 [file2 ...]\n", path.Base(os.Args[0]))
-        flag.PrintDefaults()
-        return
-    }
+	if help {
+		fmt.Printf("Usage: %s [-Hchp] file1 [file2 ...]\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+		return
+	}
 
-    for _, file := range files {
-        wg.Add(1)
-        go SendFile(file, compressed, host, port, &wg)
-    }
+	for _, file := range files {
+		wg.Add(1)
+		go SendFile(file, compressed, host, port, &wg)
+	}
 
-    wg.Wait()
+	wg.Wait()
 }
--- ft_server.go
+++ ft_server.go
@@ -4,23 +4,23 @@
 package main
 
 import (
+	"./util"
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
-    "path"
-    "errors"
+	"path"
 	"strconv"
-    "./util"
 )
 
 func ParseParams() (port int, dst string, help bool) {
 	p := flag.Int("p", 12345, "listen port")
 	d := flag.String("d", ".", "target direcotry to store received file(s)")
-    h := flag.Bool("h", false, "print help information")
+	h := flag.Bool("h", false, "print help information")
 	flag.Parse()
 
 	return *p, *d, *h
@@ -44,86 +44,86 @@ func SendOk(conn net.Conn) {
 }
 
 func Checksum(file *os.File, fullname string, size int64, md5 string, conn net.Conn) {
-    // check file size
-    fi, err := file.Stat()
-    if err != nil {
-        SendError(conn, "Stat() failed", err.Error())
-        return
-    }
-    if size != fi.Size() {
+	// check file size
+	fi, err := file.Stat()
+	if err != nil {
+		SendError(conn, "Stat() failed", err.Error())
+		return
+	}
+	if size != fi.Size() {
 		log.Printf("Receiving %s failed\n", fullname)
-        SendError(conn, "different size", fmt.Sprintf("sent: %d bytes, received: %d bytes", size, fi.Size()))
-        return
-    }
+		SendError(conn, "different size", fmt.Sprintf("sent: %d bytes, received: %d bytes", size, fi.Size()))
+		return
+	}
 
-    // check file md5
-    MD5, err := util.MD5(file)
-    if err != nil {
-        log.Printf(err.Error())
-    }
+	// check file md5
+	MD5, err := util.MD5(file)
+	if err != nil {
+		log.Printf(err.Error())
+	}
 
 	if md5 == MD5 && MD5 != "nil" {
-        log.Printf("Receiving %s successed, md5(%s) matched", fullname, md5)
+		log.Printf("Receiving %s successed, md5(%s) matched", fullname, md5)
 		SendOk(conn)
-    } else if md5 != MD5 && md5 != "nil" && MD5 != "nil" {
+	} else if md5 != MD5 && md5 != "nil" && MD5 != "nil" {
 		log.Printf("Receiving %s failed\n", fullname)
-        SendError(conn, "different md5", fmt.Sprintf("sent: %s, received: %s", md5, MD5))
+		SendError(conn, "different md5", fmt.Sprintf("sent: %s, received: %s", md5, MD5))
 	} else {
-        log.Printf("Warnig: please check the md5 of %s manually\n", fullname)
-    }
+		log.Printf("Warnig: please check the md5 of %s manually\n", fullname)
+	}
 }
 
 func ReceiveData(conn net.Conn, hdr, buf []byte) (int, error) {
-    // read header
-    hl, err := conn.Read(hdr)
-    if err != nil {
-        return 0, err
-    }
+	// read header
+	hl, err := conn.Read(hdr)
+	if err != nil {
+		return 0, err
+	}
 
-    if hl != util.DATA_HDR_SIZE {
-        return 0, errors.New("wrong data header")
-    }
+	if hl != util.DATA_HDR_SIZE {
+		return 0, errors.New("wrong data header")
+	}
 
-    dl, err := util.ParseHeader(hdr)
-    if err != nil || dl < 0 {
-        return 0, errors.New("wrong data header")
-    }
+	dl, err := util.ParseHeader(hdr)
+	if err != nil || dl < 0 {
+		return 0, errors.New("wrong data header")
+	}
 
-    start := 0
-    left := dl
-    for {
-        if left == 0 {
-            break
-        }
-        // read data
-        b := buf[start:left]
-        l, err := conn.Read(b)
+	start := 0
+	left := dl
+	for {
+		if left == 0 {
+			break
+		}
+		// read data
+		b := buf[start:left]
+		l, err := conn.Read(b)
 
-        if err != nil {
-            return 0, err
-        }
-        start += l
-        left -= l
-    }
+		if err != nil {
+			return 0, err
+		}
+		start += l
+		left -= l
+	}
 
-    return dl, nil
+	return dl, nil
 }
 
 func HandleClient(conn net.Conn, dst string) {
 	addr := conn.RemoteAddr()
 	log.Printf("%s connected to the server\n", addr.String())
 
-    var meta util.FileMeta
+	var meta util.FileMeta
 	metabuf := make([]byte, util.META_BUF_SIZE)
 
-    // read meta from network
-    // meta fields:
-    //  filename
-    //  filesize
-    //  filemode
-    //  md5sum
-    //  compressed
-    //  algorithm
+	// read meta from network
+	// meta fields:
+	//  filename
+	//  filesize
+	//  filemode
+	//  md5sum
+	//  compressed
+	//  algorithm
 
 	metalen, err := conn.Read(metabuf)
 	if err != nil && err != io.EOF {
@@ -136,83 +136,83 @@ func HandleClient(conn net.Conn, dst string) {
 		return
 	}
 
-    err = util.ParseMeta(metabuf, &meta)
-    if err != nil {
+	err = util.ParseMeta(metabuf, &meta)
+	if err != nil {
 		SendError(conn, err.Error(), "must be a bug")
-        return
-    }
+		return
+	}
 
 	// create target file
 	filename := dst + "/" + meta.Name
-    file, err := os.OpenFile(filename, os.O_RDWR|os.O_TRUNC|os.O_CREATE, meta.Mode) 
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_TRUNC|os.O_CREATE, meta.Mode)
 	if err != nil {
 		SendError(conn, "Create() failed", err.Error())
 		return
 	}
 	defer file.Close()
 
-    //log.Printf("received meta:\n%v", meta)
+	//log.Printf("received meta:\n%v", meta)
 
-    var fw *bufio.Writer
-    if meta.Compressed == "yes" {
-        // decompress the received data stream and write to the target file
-        dw := util.DecompressWriter(file)
-        fw = bufio.NewWriter(dw)
-        defer dw.Close()
-    }  else {
-        fw = bufio.NewWriter(file)
-    }
+	var fw *bufio.Writer
+	if meta.Compressed == "yes" {
+		// decompress the received data stream and write to the target file
+		dw := util.DecompressWriter(file)
+		fw = bufio.NewWriter(dw)
+		defer dw.Close()
+	} else {
+		fw = bufio.NewWriter(file)
+	}
 
 	// receive file contents
-    var received int64 = 0
+	var received int64 = 0
 	buf := make([]byte, util.DATA_BUF_SIZE)
-    hdr := make([]byte, util.DATA_HDR_SIZE)
+	hdr := make([]byte, util.DATA_HDR_SIZE)
 
-    for {
-        n, err := ReceiveData(conn, hdr, buf)
-        if err != nil {
-            SendError(conn, "ReceiveData() failed", err.Error())
-            return
-        }
-        if n == 0 {
-            break
-        }
-        received += int64(n)
-        //fmt.Printf("[%d, %d]--", n, received)
+	for {
+		n, err := ReceiveData(conn, hdr, buf)
+		if err != nil {
+			SendError(conn, "ReceiveData() failed", err.Error())
+			return
+		}
+		if n == 0 {
+			break
+		}
+		received += int64(n)
+		//fmt.Printf("[%d, %d]--", n, received)
 
-        wl, err := fw.Write(buf[:n])
-        if err != nil || wl != n {
-            SendError(conn, "Write() failed", err.Error())
-            return
-        }
-    }
+		wl, err := fw.Write(buf[:n])
+		if err != nil || wl != n {
+			SendError(conn, "Write() failed", err.Error())
+			return
+		}
+	}
 
-    if err = fw.Flush(); err != nil {
-        SendError(conn, "Flush() failed", err.Error())
-        return
-    }
+	if err = fw.Flush(); err != nil {
+		SendError(conn, "Flush() failed", err.Error())
+		return
+	}
 
-    Checksum(file, filename, meta.Size, meta.Md5, conn)
+	Checksum(file, filename, meta.Size, meta.Md5, conn)
 }
 
 func main() {
 	port, dst, help := ParseParams()
 
-    if help {
-        fmt.Printf("Usage: %s [-dhp]\n", path.Base(os.Args[0]))
-        flag.PrintDefaults()
-        return
-    }
+	if help {
+		fmt.Printf("Usage: %s [-dhp]\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+		return
+	}
 
-    fi, err := os.Stat(dst)
-    if err != nil {
+	fi, err := os.Stat(dst)
+	if err != nil {
 		log.Printf("Stat() failed: %s\n", err.Error())
-        return
-    }
-    if !fi.IsDir() {
+		return
+	}
+	if !fi.IsDir() {
 		log.Printf("%s is not a directory\n", err.Error())
-        return
-    }
+		return
+	}
 
 	ln, err := net.Listen("tcp", string(":"+strconv.Itoa(port)))
 	if err != nil {
